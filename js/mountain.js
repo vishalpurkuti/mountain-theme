9e8d2ac78e1942baa1fc02ff47d2c9fdafb61567
vishalpurkuti <bishalkan6a@gmail.com>
2020-06-24 19:26:18 +0545

    css added

--- js/mountain.js
+++ js/mountain.js
@@ -1,4 +1,14 @@
-function hideonscroll() {
-    document.getElementById('notch').style.opacity = '0.0';
-    document.getElementById('test').style.opacity = '0.0';
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById('notch').style.opacity = '1.0';
+        document.getElementById('test').style.opacity = '1.0';
+
+    } else {
+        document.getElementById('notch').style.opacity = '0.0';
+        document.getElementById('test').style.opacity = '0.0';
+    }
+    prevScrollpos = currentScrollPos;
 }
